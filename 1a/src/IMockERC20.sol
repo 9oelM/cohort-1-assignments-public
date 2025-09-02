3e72aa4186312b9cc52572dee031401b2015af2a
9oelM <hj923@hotmail.com>
2025-09-02 21:30:03 +0900

    chore: use sol 0.8.13

--- 1a/src/IMockERC20.sol
+++ 1a/src/IMockERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.13;
 
 // DO NOT change the interface
 interface IMockERC20 {
--- 1a/src/MockERC20.sol
+++ 1a/src/MockERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IMockERC20} from "./IMockERC20.sol";
